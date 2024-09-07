afd5cfb0bca06b9437be0def0ffe957ebaa9c222
Bijeesh M <bijeeshh.m@gmail.com>
2024-09-07 13:11:22 +0530

    connected to mongodb atlas

--- server.js
+++ server.js
@@ -5,12 +5,20 @@ const tenderRoute = require("./routes/tender");
 const dbConnect = require("./config/db");
 const cookieParser = require("cookie-parser");
 
+const Tender = require("./models/tender");
+const User = require("./models/user");
+
 const app = express();
 
 app.use(express.json());
-app.use(cookieParser())
+app.use(cookieParser());
 
 dbConnect();
+
+app.get("/", async (req, res) => {
+    await Tender.find();
+    await User.find();
+});
 
 app.use("/user", userRoute);
 app.use("/tender", tenderRoute);
